c99b841796dc6d0abe7ba170d61f1a5d60b1766e
frankichi <francurcia@gmail.com>
2026-04-24 23:51:22 -0500

    Delete quick-rescue directory

--- quick-rescue/backend/src/controllers/sos.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-import * as service from '../services/sos.service';
-
-export const disparar = async (req: Request, res: Response): Promise<void> => {
-  const data = await service.dispararSOS(req.user!.uid, req.body);
-  res.json({ success: true, data });
-};
